f15663222cb1c4dbfc2b61c63df8f8dd412f3ad4
username_anjastabuni <reblytabuni2004@gmail.com>
2025-02-19 18:51:49 +0700

    buat fungsi simpan data register dengan bcrypt

--- auth_demo/index.js
+++ auth_demo/index.js
@@ -1,13 +1,44 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const User = require("./models/user");
+
+mongoose
+  .connect("mongodb://127.0.0.1/auth_demo")
+  .then((result) => {
+    console.log("connect to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
+
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hashPassword = bcrypt.hashSync(password, 10);
+  const user = new User({
+    username,
+    password: hashPassword,
+  });
+  await user.save();
+  res.redirect("/");
 });
 
 app.get("/admin", (req, res) => {
